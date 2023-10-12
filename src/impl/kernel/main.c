8e50b8699f606927976868d7412cf9aaadeea2d6
ACCESS-DENIDE <psvitaday2@gmail.com>
2023-10-12 13:39:21 +0300

    NewPagingINI

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,11 +1,6 @@
 #include "print.h"
-#include "InterrProc.h"
 
 void kernel_main_c(){
-    clear_print();
-    intr_init();
-    intr_start();
-    intr_enable();
 
    
     set_color(COLOR_GREEN, COLOR_BLACK);
--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -7,6 +7,7 @@ section .text
 ;processor_word_length
 bits 32
 start:
+    cli 
     mov esp, stack_top
 
     call check_multiboot
@@ -16,8 +17,9 @@ start:
     call setup_paging
     call enable_paiging_longmode
 
-    lgdt [gdt64.pointer]
-    jmp gdt64.code_segment:long_mode_start
+    lgdt [GDT64.Pointer]         ; Load the 64-bit global descriptor table.
+    mov ax, GDT64.Data  
+    jmp GDT64.Code:long_mode_start
 
     ;print_OK_32_bit
     mov dword [0xb8000], 0x2f4b2f4f
@@ -77,48 +79,50 @@ error_print:
 
 
 setup_paging:
-    mov eax, page_tab_l3
-    or eax, 0b11
-    mov [page_tab_l4], eax
+    mov edi, 0x1000    ; Set the destination index to 0x1000.
+    mov cr3, edi       ; Set control register 3 to the destination index.
+    xor eax, eax       ; Nullify the A-register.
+    mov ecx, 4096      ; Set the C-register to 4096.
+    rep stosd          ; Clear the memory.
+    mov edi, cr3  
 
-    mov eax, page_tab_l2
-    or eax, 0b11
-    mov [page_tab_l3], eax
+    mov DWORD [edi], 0x2003      ; Set the uint32_t at the destination index to 0x2003.
+    add edi, 0x1000              ; Add 0x1000 to the destination index.
+    mov DWORD [edi], 0x3003      ; Set the uint32_t at the destination index to 0x3003.
+    add edi, 0x1000              ; Add 0x1000 to the destination index.
+    mov DWORD [edi], 0x4003      ; Set the uint32_t at the destination index to 0x4003.
+    add edi, 0x1000    
 
-    mov ecx, 0
+    mov ebx, 0x00000003          ; Set the B-register to 0x00000003.
+    mov ecx, 512                 ; Set the C-register to 512.
+ 
+.SetEntry:
+    mov DWORD [edi], ebx         ; Set the uint32_t at the destination index to the B-register.
+    add ebx, 0x1000              ; Add 0x1000 to the B-register.
+    add edi, 8                   ; Add eight to the destination index.
+    loop .SetEntry   
 
-.loop:
-
-    mov eax, 0x200000
-    mul ecx
-    or eax, 0b10000011
-    mov [page_tab_l2+ecx*8], eax
-
-    inc ecx
-    cmp ecx, 512
-    jne .loop
+    mov eax, cr4                 ; Set the A-register to control register 4.
+    or eax, 1 << 5               ; Set the PAE-bit, which is the 6th bit (bit 5).
+    mov cr4, eax    
 
     ret
 
 enable_paiging_longmode:
-    mov eax, page_tab_l4
-    mov cr3, eax
+    mov ecx, 0xC0000080          ; Set the C-register to 0xC0000080, which is the EFER MSR.
+    rdmsr                        ; Read from the model-specific register.
+    or eax, 1 << 8               ; Set the LM-bit which is the 9th bit (bit 8).
+    wrmsr     
 
-    mov eax, cr4
-    or eax, 1<<5
-    mov cr4, eax
+    mov eax, cr0                 ; Set the A-register to control register 0.
+    or eax, 1 << 31 | 1 << 0     ; Set the PG-bit, which is the 31nd bit, and the PM-bit, which is the 0th bit.
+    mov cr0, eax   
 
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1<<8
-    wrmsr
-
-    mov eax, cr0
-    or eax, 1<<31
-    mov cr0, eax
+   
 
     ret
     
+
 
 section .bss
 
@@ -135,11 +139,39 @@ stack_bottom:
     resb 4096*4
 stack_top:
 
-section .rodata
-gdt64:
-    dq 0
-.code_segment: equ $ - gdt64
-    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) 
-.pointer
-    dw $ - gdt64 -1
-    dq gdt64
+
+
+; Access bits
+PRESENT        equ 1 << 7
+NOT_SYS        equ 1 << 4
+EXEC           equ 1 << 3
+DC             equ 1 << 2
+RW             equ 1 << 1
+ACCESSED       equ 1 << 0
+ 
+; Flags bits
+GRAN_4K       equ 1 << 7
+SZ_32         equ 1 << 6
+LONG_MODE     equ 1 << 5
+ 
+GDT64:
+    .Null: equ $ - GDT64
+        dq 0
+    .Code: equ $ - GDT64
+        dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
+        db 0                                        ; Base (mid, bits 16-23)
+        db PRESENT | NOT_SYS | EXEC | RW            ; Access
+        db GRAN_4K | LONG_MODE | 0xF                ; Flags & Limit (high, bits 16-19)
+        db 0                                        ; Base (high, bits 24-31)
+    .Data: equ $ - GDT64
+        dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
+        db 0                                        ; Base (mid, bits 16-23)
+        db PRESENT | NOT_SYS | RW                   ; Access
+        db GRAN_4K | SZ_32 | 0xF                    ; Flags & Limit (high, bits 16-19)
+        db 0                                        ; Base (high, bits 24-31)
+    .TSS: equ $ - GDT64
+        dd 0x00000068
+        dd 0x00CF8900
+    .Pointer:
+        dw $ - GDT64 - 1
+        dq GDT64
